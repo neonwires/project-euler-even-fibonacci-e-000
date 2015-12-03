eb21eb90bf9b22d156a2434c5ad851b64a462c4b
neonwires <neonwires@gmail.com>
2015-12-03 21:46:39 +0000

    nitrous-backup at Thu Dec  3 21:46:39 UTC 2015

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,22 @@
 # Implement your procedural solution here!
+def fib(n)
+    if n == 0
+        return 0
+    end
+    if n == 1
+        return 1
+    end
+    if n >= 2
+        return fib(n-1) + (fib(n-2))
+    end
+end
+
+def even_fibonacci_sum(limit)
+  fib_array = []
+  for i in 1..limit - 1
+    if fib(i) < limit && fib(i) % 2 == 0
+    fib_array.push(fib(i))
+    end
+  end
+  fib_array.reduce(:+)
+end
